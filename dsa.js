c63a022d2bea739fb49548ba7aa8025bf4e89dc2
jxlee007 <jimmyfalco007@gmail.com>
2026-06-08 19:21:30 +0530

    Add DSA Big O notation comments in dsa.js

---
+++ dsa.js
@@ -0,0 +1,24 @@
+/* DSA 
+
+Big O notation = A way to describe 
+how fast the algo runs in relation to input
+
+n = input
+kinds of algo notations with differ time complexities
+
+O(n) algo
+- when one input or one loop 
+- the loop will runs run as many times as input
+
+O(n^2)
+- when there is a nested loop
+- o of n is much efficient then this in time complexity
+
+O(1) - constant time
+- when there is no loop only input
+- or when  loop with fixed output
+- most efficient algo
+
+
+
+*/
